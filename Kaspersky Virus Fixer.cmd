7c7c61b4523dca12a6c089f0a6004a6b885979d0
BroSpek <BroSpek@users.noreply.github.com>
2017-12-29 23:58:12 +0800

    Update 1.3.1
    - add admin ask
    - code cleanup
    - minor tweak

--- Kaspersky Virus Fixer.cmd
+++ Kaspersky Virus Fixer.cmd
@@ -1,177 +1,179 @@
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::                                                       ::
-::  Kaspersky Virus Fixer v1.2 by Abd Halim @ Angah ICT  ::
-::                                                       ::
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@echo off
-set title=Kaspersky Virus Fixer v1.2
-title %title%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::                                                         ::
+::  Kaspersky Virus Fixer v1.3.1 by Abd Halim @ Angah ICT  ::
+::                                                         ::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@ECHO off
+COLOR 09
+SET title=KASPERSKY VIRUS FIXER v1.3.1
+TITLE %title%
+
+:: invoke admin
+PUSHD "%CD%"
+CD /d "%~dp0"
+( NET file || ( POWERSHELL -command start-process '%0' -verb RUNAS -argumentlist '%* '& EXIT /b ) ) >NUL 2>NUL
 
 ::########### SCRIPT GLOBAL VARIABLES #############::
-set kis=kaspersky internet security 2017
-set kis_bat="*Kaspersky Internet Security 2017).bat"
-set usr_start=%appdata%\Microsoft\Windows\Start Menu\Programs\Startup
-set sys_start=%programdata%\Microsoft\Windows\Start Menu\Programs\Startup
-set er1=A&set er2=B&set er3=C&set er4=D&set er5=E&set er6=F&set er7=G&set er8=H&set er9=I&set er10=J&set er11=K&set er12=L&set er13=M&set er14=N&set er15=O&set er16=P&set er17=Q&set er18=R&set er19=S&set er20=T&set er21=U&set er22=V&set er23=W&set er24=X&set er25=Y&set er26=Z
+SET null=
+SET kis=kaspersky internet security 2017
+SET kis_bat="*Kaspersky Internet Security 2017).bat"
+SET startup=Microsoft\Windows\Start Menu\Programs\Startup
+SET usr_start=%APPDATA%\%startup%
+SET sys_start=%PROGRAMDATA%\%startup%
+SET er1=A& SET er2=B& SET er3=C& SET er4=D& SET er5=E& SET er6=F& SET er7=G& SET er8=H& SET er9=I& SET er10=J& SET er11=K& SET er12=L& SET er13=M& SET er14=N& SET er15=O& SET er16=P& SET er17=Q& SET er18=R& SET er19=S& SET er20=T& SET er21=U& SET er22=V& SET er23=W& SET er24=X& SET er25=Y& SET er26=Z
 
 ::################## MAIN SCRIPT ##################::
 ::============== Page 1 - Disclaimer ==============::
-set msg=This small script will help you to remove virus that is related to:
-call :header
-echo. "Protected by Kaspersky Internet Security 2017".
-echo.
-echo. This script comes with no warranty. Use at your own risk.
-echo.
-choice /c ax /n /m "Press [A] to agree and continue, [X] to exit: "
-if %errorlevel%==2 exit
-if %errorlevel%==1 goto choose
+SET subtitle=[ %title% ]
+SET msg=This small script will help you to remove virus that is related to:
+CALL :header
+ECHO. "Secured by Kaspersky Internet Security 2017".
+ECHO. & ECHO. This script comes with no warranty. Use at your own risk.
+ECHO. & CHOICE /c ax /n /m "Press [A] to agree and continue, [X] to exit: "
+IF %ERRORLEVEL%==2 EXIT
+IF %ERRORLEVEL%==1 GOTO choose
 
 ::============= Page 2 - Choose Action ============::
 :choose
-set msg=Choose an option:
-call :header
-echo.
-echo. Press [U] to fix USB drive.
-echo. Press [C] to convert USB drive File System into NTFS.
-echo. Press [S] to fix system.
-echo. Press [P] to check for update.
-echo. Press [X] to exit.
-echo.
-choice /c ucspx /n /m " Choose: " 
-if %errorlevel%==5 exit
-if %errorlevel%==4 start https://github.com/BroSpek/Kaspersky-Virus-Fixer/releases/latest
-if %errorlevel%==3 call :clean_sys
-if %errorlevel%==2 call :convert_fs
-if %errorlevel%==1 call :clean_usb
-goto choose
-exit /b %errorlevel%
+SET subtitle=[ %title% ]
+SET msg=Choose an option:
+CALL :header & ECHO.
+ECHO. Press [U] to fix USB drive.
+ECHO. Press [C] to convert USB drive File System into NTFS.
+ECHO. Press [S] to fix system.
+ECHO. Press [P] to check for update.
+ECHO. Press [X] to exit.
+ECHO. & CHOICE /c ucspx /n /m " Choose: " 
+IF %ERRORLEVEL%==5 EXIT
+IF %ERRORLEVEL%==4 START https://github.com/BroSpek/Kaspersky-Virus-Fixer/releases/latest
+IF %ERRORLEVEL%==3 CALL :clean_sys
+IF %ERRORLEVEL%==2 CALL :convert_fs
+IF %ERRORLEVEL%==1 CALL :clean_usb
+GOTO choose
+EXIT /b %ERRORLEVEL%
 
 ::################### FUNCTIONS ###################::
 :clean_usb
-call :header
-echo.
-echo. Detected USB drive(s):
-setlocal
-for /f "usebackq skip=1 tokens=*" %%i in (`wmic logicaldisk where drivetype^=2 get deviceid^, volumename`) do echo: %%i
-endlocal
-::------
-setlocal enableDelayedExpansion
-choice /c ABCDEFGHIJKLMNOPQRSTUVWXYZ /n /m "Choose a USB drive to fix or press A to return: "
-set root=!er%errorlevel%!
-if %root% equ A goto choose
-if %root% lss D set msg=ERROR: Drive letter must be between D and Z. && goto clean_usb
-if %root% gtr Z set msg=ERROR: Drive letter must be between D and Z. && goto clean_usb
-if exist "%root%:\" (
-	echo Drive letter exist!
-) else (
-	set msg=ERROR: Drive letter %root%: is currently unused.
-	goto clean_usb
-)
-:: fixing...
-set svi=%root%:\system volume information
-set msg=INFO: USB drive %root%: selected.
-call :header
-cd /d %root%:\
-if exist %kis_bat% del /f %kis_bat%
-cd /d "%svi%"
-dir /A /B "%svi%" | findstr .*>NUL && attrib -h -s /d
-if exist "%kis%" rd /s /q "%kis%" 2>NUL
-if exist "indexervolumeguid" del "indexervolumeguid" 2>NUL
-if exist "wpsettings.dat" del "wpsettings.dat" 2>NUL
+SET subtitle=[ FIX USB DRIVE ]
+CALL :header
+ECHO. & ECHO. Detected USB drive(s):
+SETLOCAL
+	FOR /f "usebackq skip=1 tokens=*" %%i in ( `WMIC logicaldisk WHERE drivetype^=2 GET deviceid^, volumename^, filesystem` ) do ECHO: %%i
+ENDLOCAL
 
-for /d %%i in ("%svi%\*") do move "%%i" "%root%:\%%~nxi"
-dir /A /B "%svi%" | findstr .*>NUL && move "%svi%\*.*" "%root%:\"
-echo.
-echo. RESULT: Done. Please check drive %root%: content.
-endlocal
-::------
-call :final
-goto choose
-exit /b %errorlevel%
+SETLOCAL enableDelayedExpansion
+	CHOICE /c ABCDEFGHIJKLMNOPQRSTUVWXYZ /n /m "Choose a USB drive to fix or press A to return: "
+	SET root=!er%ERRORLEVEL%!
+	IF %root% EQU A GOTO choose
+	::IF %root% LSS D SET msg=ERROR: Drive letter must be between D and Z. && GOTO clean_usb
+	::IF %root% GTR Z SET msg=ERROR: Drive letter must be between D and Z. && GOTO clean_usb
+	CALL :check_remv
+	IF %remv%==n SET msg=ERROR: Choose a drive letter from the list. && GOTO clean_usb
+
+	:: fixing...
+	SET svi=%root%:\system volume information
+	SET msg=INFO: USB drive %root%: selected.
+	CALL :check_ntfs
+	IF %ntfs%==y set msg=ERROR: I cannot process NTFS drive. Sorry. && GOTO clean_usb
+	CALL :header
+	CD /d %root%:\
+	IF EXIST %kis_bat% DEL /f %kis_bat%
+	CD /d "%svi%"
+	DIR /A /B "%svi%" | FINDSTR .*>NUL && ATTRIB -h -s /d
+	IF EXIST "%kis%" RD /s /q "%kis%" >NUL 2>NUL
+	IF EXIST "indexervolumeguid" DEL "indexervolumeguid" >NUL 2>NUL
+	IF EXIST "wpsettings.dat" DEL "wpsettings.dat" >NUL 2>NUL
+	FOR /d %%i in ( "%svi%\*" ) do MOVE "%%i" "%root%:\%%~nxi"
+	DIR /A /B "%svi%" | FINDSTR .*>NUL && MOVE "%svi%\*.*" "%root%:\"
+	ECHO. & ECHO. RESULT: Done. Please check drive %root%: content.
+ENDLOCAL
+
+CALL :final
+GOTO choose
+EXIT /b %ERRORLEVEL%
 
 :convert_fs
-call :header
-echo.
-echo. Detected USB drive(s):
-setlocal
-for /f "usebackq skip=1 tokens=*" %%i in (`wmic logicaldisk where drivetype^=2 get deviceid^, volumename^, filesystem`) do echo: %%i
-endlocal
-::------
-setlocal enableDelayedExpansion
-echo.
-echo. WARNING: Please backup your files before doing this.
-echo.
-choice /c ABCDEFGHIJKLMNOPQRSTUVWXYZ /n /m "Choose a USB drive to convert or press A to return: "
-set root=!er%errorlevel%!
-if %root% equ A goto choose
-if %root% lss D set msg=ERROR: Drive letter must be between D and Z. && goto convert_fs
-if %root% gtr Z set msg=ERROR: Drive letter must be between D and Z. && goto convert_fs
-if exist "%root%:\" (
-	echo.
-) else (
-	set msg=ERROR: Drive letter %root%: is currently unused.
-	goto convert_fs
-)
-:: converting...
-wmic logicaldisk where caption="%root%:" get filesystem|find "NTFS">nul && set ntfs=y || set ntfs=n
-if %ntfs%==y set msg=ERROR: Drive %root%: is already in NTFS format. && goto convert_fs
-cls
-echo.
-echo. This may take a while. Please be patient.
-echo.
-set/p<nul =1. Checking and fixing file system...&chkdsk %root%: /f /x>nul
-echo.
-echo.
-set/p<nul =2. Converting file system...&convert %root%: /fs:ntfs>nul
-wmic logicaldisk where caption="%root%:" get filesystem|find "NTFS">nul && set ntfs=y || set ntfs=n
-if %ntfs%==y (echo. Done.) else (echo. Ooops. Conversion failed, please try again.)
-echo.
-::------
-call :final
-goto choose
-exit /b %errorlevel%
+SET subtitle=[ CONVERT USB DRIVE ]
+CALL :header
+ECHO. & ECHO. Detected USB drive(s):
+SETLOCAL
+	FOR /f "usebackq skip=1 tokens=*" %%i in ( `WMIC logicaldisk WHERE drivetype^=2 GET deviceid^, volumename^, filesystem` ) do ECHO: %%i
+ENDLOCAL
+
+SETLOCAL enableDelayedExpansion
+	ECHO. WARNING: Please backup your files before doing this.
+	ECHO. & CHOICE /c ABCDEFGHIJKLMNOPQRSTUVWXYZ /n /m "Choose a USB drive to convert or press A to return: "
+	SET root=!er%ERRORLEVEL%!
+	IF %root% EQU A GOTO choose
+	::IF %root% LSS D SET msg=ERROR: Drive letter must be between D and Z. && GOTO convert_fs
+	::IF %root% GTR Z SET msg=ERROR: Drive letter must be between D and Z. && GOTO convert_fs
+	CALL :check_remv
+	IF %remv%==n SET msg=ERROR: Choose a drive letter from the list. && GOTO convert_fs
+
+	:: converting...
+	CALL :check_ntfs
+	IF %ntfs%==y SET msg=ERROR: Drive %root%: is already in NTFS format. && GOTO convert_fs
+	CLS
+	ECHO. & ECHO. This may take a while. Please be patient.
+	ECHO. & SET /p <NUL=1. Checking and fixing file system... & CHKDSK %root%: /f /x >NUL 2>NUL
+	ECHO. & ECHO. & SET /p <NUL=2. Converting file system... & CONVERT %root%: /fs:ntfs >NUL 2>NUL
+	WMIC logicaldisk WHERE caption="%root%:" GET filesystem | FIND "NTFS" >NUL && SET ntfs=y || SET ntfs=n
+	IF %ntfs%==y ( ECHO. Done. ) ELSE ( ECHO. Ooops. Conversion failed, please try again. )
+	ECHO.
+ENDLOCAL
+
+CALL :final
+GOTO choose
+EXIT /b %ERRORLEVEL%
 
 :clean_sys
-call :header
-if exist "%appdata%\%kis%" (
-	cd /d "%appdata%"
-	call :killtask "explorers.exe"
-	call :killtask "spoolsvc.exe"
-	call :killtask "spoolsvc.exe"
-	rd /s /q "%kis%"
-	cd /d "%usr_start%"
-	call :shortcut
-	cd /d "%sys_start%"
-	call :shortcut
-	set msg=RESULT: Done. Kaspersky Virus has been removed.
-) else (
-	set msg=RESULT: Done. Kaspersky Virus not found.
+SET subtitle=[ FIX SYSTEM ]
+CALL :header
+IF EXIST "%APPDATA%\%kis%" (
+	CD /d "%APPDATA%"
+	CALL :killtask "explorers.exe"
+	CALL :killtask "spoolsvc.exe"
+	CALL :killtask "spoolsvc.exe"
+	RD /s /q "%kis%"
+	CD /d "%usr_start%"
+	CALL :shortcut
+	CD /d "%sys_start%"
+	CALL :shortcut
+	SET msg=RESULT: Done. Kaspersky Virus has been removed.
+) ELSE (
+	SET msg=RESULT: Done. Kaspersky Virus not found.
 )
-call :header
-call :final
-goto choose
-exit /b %errorlevel%
+CALL :header
+CALL :final
+GOTO choose
+EXIT /b %ERRORLEVEL%
 
 :header
-cls
-echo.
-echo. %title%
-echo.
-echo. %msg%
-set "msg="
-exit /b %errorlevel%
+CLS
+ECHO. & ECHO. %subtitle%
+ECHO. & ECHO. %msg%
+SET msg=%null%
+EXIT /b %ERRORLEVEL%
+
+:check_remv
+WMIC logicaldisk WHERE caption="%root%:" GET drivetype | FIND "2">NUL && SET remv=y || SET remv=n
+EXIT /b %ERRORLEVEL%
+
+:check_ntfs
+WMIC logicaldisk WHERE caption="%root%:" GET filesystem | FIND "NTFS">NUL && SET ntfs=y || SET ntfs=n
+EXIT /b %ERRORLEVEL%
 
 :killtask
-tasklist /FI "IMAGENAME eq %*" 2>NUL | find /I /N "%*">NUL 2>NUL
-if "%ERRORLEVEL%"=="0" taskkill /f /im %*
-exit /b %errorlevel%
+TASKLIST /fi "imagename eq %*" 2>NUL | FIND /i /n "%*" >NUL 2>NUL
+IF "%ERRORLEVEL%"=="0" taskkill /f /im %*
+EXIT /b %ERRORLEVEL%
 
 :shortcut
-if exist explorers.lnk del explorers.lnk
-if exist svhost.lnk del svhost.lnk
-if exist spoolsvc.lnk del spoolsvc.lnk
-exit /b %errorlevel%
+IF EXIST explorers.lnk DEL explorers.lnk
+IF EXIST svhost.lnk DEL svhost.lnk
+IF EXIST spoolsvc.lnk DEL spoolsvc.lnk
+EXIT /b %ERRORLEVEL%
 
 :final
-echo.
-set/p<nul = Going back to main menu, press any key . . .&pause>nul
+ECHO. & SET /p <NUL=Going back to main menu, press any key . . . & PAUSE >NUL 2>NUL
+EXIT /b %ERRORLEVEL%
